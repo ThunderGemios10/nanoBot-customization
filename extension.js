4affc410dede1b8e779929bcb5fecbf2a5a17821
Romarjosh Villamor <marilyndris@gmail.com>
2015-02-27 23:11:43 -0800

    Modifications.

--- extension.js
+++ extension.js
@@ -52,12 +52,12 @@
 
     //Change the bots default settings and make sure they are loaded on launch
 
-    localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "basicBot",
+    localStorage.setItem("nanoBotsettings", JSON.stringify({
+        botName: "nanoBot",
         language: "english",
-        chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
+        chatLink: "https://rawgit.com/ThunderGemios10/nanoBot/master/lang/en.json",
         startupCap: 1, // 1-200
-        startupVolume: 0, // 0-100
+        startupVolume: 50, // 0-100
         startupEmoji: false, // true or false
         maximumAfk: 120,
         afkRemoval: true,
@@ -104,12 +104,12 @@
         songstats: true,
         commandLiteral: "!",
         blacklists: {
-            NSFW: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
-            OP: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleOPlist.json"
+            NSFW: "https://rawgit.com/ThunderGemios10/nanoBot-customization/master/blacklists/ExampleNSFWlist.json",
+            OP: "https://rawgit.com/ThunderGemios10/nanoBot-customization/master/blacklists/ExampleOPlist.json"
         }
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript('https://rawgit.com/Yemasthui/basicBot/master/basicBot.js', extend);
+    $.getScript('https://rawgit.com/ThunderGemios10/nanoBot/master/nanoBot.js', extend);
 
 }).call(this);
